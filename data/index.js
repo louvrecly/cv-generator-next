900c17334c305afce174c172064281f194fda56f
Louvre Chow <louvrecly@gmail.com>
2022-06-17 23:37:12 -0400

    Create data constant

---
+++ data/index.js
@@ -0,0 +1,21 @@
+const data = {
+  name: 'User Name',
+  contact: {
+    mobile: '+0 (000) 000 0000',
+    email: 'username@email.com'
+  },
+  references: [
+    {
+      key: 'github',
+      label: 'GitHub',
+      link: 'https://github.com/username'
+    },
+    {
+      key: 'gitlab',
+      label: 'GitLab',
+      link: 'https://gitlab.com/username'
+    }
+  ]
+}
+
+export default data
