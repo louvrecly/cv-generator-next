f86ee94afb19155efa3efbcfd04cfc21ae25d84e
Louvre Chow <louvrecly@gmail.com>
2022-06-18 18:09:16 -0400

    Replace useState, useEffect and fetch with useSWR and fetcher on home page

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,17 @@
-import { useState, useEffect } from 'react'
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function Home() {
-  const [data, setData] = useState(null)
-  const [busy, setBusy] = useState(false)
-
-  useEffect(() => {
-    setBusy(true)
-
-    try {
-      fetch('/api/user')
-        .then(res => res.json())
-        .then(data => setData(data))
-        .finally(() => setBusy(false))
-    } catch (err) {
-      console.log({ err })
-    }
-  }, [])
+  const { data, error } = useSWR('api/user', fetcher)
 
   return (
     <div className="mx-8 py-12 h-full max-w-screen-sm break-words md:mx-auto lg:max-w-[960px]">
       {
-        busy
-          ? <p>Loading...</p>
+        error
+          ? <p>Failed to Load Data</p>
           : !data
-          ? <p>No data available</p>
+          ? <p>Loading...</p>
           : JSON.stringify(data)
       }
     </div>
