a2d7483462d4a0d04bce93cf6fa89166036f02e2
Louvre Chow <louvrecly@gmail.com>
2022-06-18 00:40:20 -0400

    Run npx tailwindcss init -p and set up tailwind.config and postcss.config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
