65a42a4d31b12b0d4f4df9d1324dca77620f9440
Louvre Chow <louvrecly@gmail.com>
2022-06-18 17:40:49 -0400

    Create data and busy state and fetch /api/user in useEffect on home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
-import data from 'data'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [data, setData] = useState(null)
+  const [busy, setBusy] = useState(false)
+
+  useEffect(() => {
+    setBusy(true)
+
+    try {
+      fetch('/api/user')
+        .then(res => res.json())
+        .then(data => setData(data))
+        .finally(() => setBusy(false))
+    } catch (err) {
+      console.log({ err })
+    }
+  }, [])
+
   return (
     <div className="mx-8 py-12 h-full max-w-screen-sm break-words md:mx-auto lg:max-w-[960px]">
-      {JSON.stringify(data)}
+      {
+        busy
+          ? <p>Loading...</p>
+          : !data
+          ? <p>No data available</p>
+          : JSON.stringify(data)
+      }
     </div>
   )
 }
