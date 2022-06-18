01a3993807a147427b78f34bcfafba2f79437a59
Louvre Chow <louvrecly@gmail.com>
2022-06-17 23:41:07 -0400

    Create info.json and import it to data constant

--- data/index.js
+++ data/index.js
@@ -1,21 +1,5 @@
-const data = {
-  name: 'User Name',
-  contact: {
-    mobile: '+0 (000) 000 0000',
-    email: 'username@email.com'
-  },
-  references: [
-    {
-      key: 'github',
-      label: 'GitHub',
-      link: 'https://github.com/username'
-    },
-    {
-      key: 'gitlab',
-      label: 'GitLab',
-      link: 'https://gitlab.com/username'
-    }
-  ]
-}
+import info from './info.json'
+
+const data = { info }
 
 export default data
