42206a9f999f5afc63587fa9ec89c0225fa85c40
Louvre Chow <louvrecly@gmail.com>
2022-06-18 18:23:10 -0400

    Add references fetch with useSWR on home page

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,30 @@ import useSWR from 'swr'
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function Home() {
-  const { data, error } = useSWR('api/user', fetcher)
+  const userRes = useSWR('api/user', fetcher)
+  const referencesRes = useSWR('api/references', fetcher)
 
   return (
     <div className="mx-8 py-12 h-full max-w-screen-sm break-words md:mx-auto lg:max-w-[960px]">
-      {
-        error
-          ? <p>Failed to Load Data</p>
-          : !data
-          ? <p>Loading...</p>
-          : JSON.stringify(data)
-      }
+      <div>
+        {
+          userRes.error
+            ? <p>Failed to Load Data</p>
+            : !userRes.data
+            ? <p>Loading...</p>
+            : JSON.stringify(userRes.data)
+        }
+      </div>
+
+      <div>
+        {
+          referencesRes.error
+            ? <p>Failed to Load Data</p>
+            : !referencesRes.data
+            ? <p>Loading...</p>
+            : JSON.stringify(referencesRes.data)
+        }
+      </div>
     </div>
   )
 }
