a7806d410eb32ea4b6449920587492102310d4c0
Louvre Chow <louvrecly@gmail.com>
2022-06-17 23:36:14 -0400

    Import DefaultLayout to wrap Component in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import DefaultLayout from 'layouts/default'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DefaultLayout>
+      <Component {...pageProps} />
+    </DefaultLayout>
+  )
 }
 
 export default MyApp
