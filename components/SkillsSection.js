59a7122a01245a6da93df9141e9bd46aa9396d03
Louvre Chow <louvrecly@gmail.com>
2022-06-19 18:25:47 -0400

    Create SkillsSection component

---
+++ components/SkillsSection.js
@@ -0,0 +1,29 @@
+export default function SkillsSection({ skills }) {
+  return (
+    <section className="py-1">
+      <h2 className="text-base uppercase underline">Skills</h2>
+
+      <ul>
+        {skills.categories.map(category => (
+          <li key={category.key} className="py-1">
+            <h3 className="font-bold text-sm">{category.label}</h3>
+
+            <ul>
+              {category.groups.map(group => (
+                <li key={group.key}>
+                  <h4 className="py-1 text-neutral-700 text-xs italic border-b border-neutral-700">{group.label}</h4>
+
+                  <ul className="py-1 flex-1 flex flex-wrap gap-1 text-xs">
+                    {group.items.map(item => (
+                      <li key={item.key} className="px-2 py-0.5 whitespace-nowrap border rounded-full shadow-sm">{item.label}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
