721c4878ee0d19af08a653eeec38cf48c2e9d5fc
Louvre Chow <louvrecly@gmail.com>
2022-06-19 14:54:31 -0400

    Create UserInfoBar component

---
+++ components/UserInfoBar.js
@@ -0,0 +1,16 @@
+export default function UserInfoBar({ user }) {
+  return (
+    <div className="py-1 flex flex-wrap justify-stretch items-center gap-x-3 text-blue-900 border-b-2 border-red-700">
+      <h1 className="flex-1 text-xl uppercase whitespace-nowrap">{user.name}</h1>
+
+      <ul>
+        {user.contacts?.map(contact => (
+          <li key={contact.key} className="flex justify-between text-xs">
+            <span className="mr-1 font-bold">{contact.label}:</span>
+            <span>{contact.value}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
