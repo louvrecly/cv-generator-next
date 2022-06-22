786dc9d06fb68ce59242ebfce9b9417d2b706072
Louvre Chow <louvrecly@gmail.com>
2022-06-22 19:30:26 -0400

    Wrap DefaultLayout with all Context.Providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
+import { UserProvider } from 'context/user'
+import { ReferencesProvider } from 'context/references'
+import { SkillsProvider } from 'context/skills'
+import { WorkExperienceProvider } from 'context/workExperience'
+import { EducationProvider } from 'context/education'
 import DefaultLayout from 'components/layouts/default'
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <DefaultLayout>
-      <Component {...pageProps} />
-    </DefaultLayout>
+    <UserProvider>
+      <ReferencesProvider>
+        <SkillsProvider>
+          <WorkExperienceProvider>
+            <EducationProvider>
+              <DefaultLayout>
+                <Component {...pageProps} />
+              </DefaultLayout>
+            </EducationProvider>
+          </WorkExperienceProvider>
+        </SkillsProvider>
+      </ReferencesProvider>
+    </UserProvider>
   )
 }
 
