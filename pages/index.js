57f844bac7588573df3055a8314754883724afc1
Louvre Chow <louvrecly@gmail.com>
2022-06-22 19:31:30 -0400

    Replace useState and useSWR with useContext on home page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,20 @@
-import { useState } from 'react'
-import useSWR from 'swr'
+import { useUserContext } from 'context/user'
+import { useReferencesContext } from 'context/references'
+import { useSkillsContext } from 'context/skills'
+import { useWorkExperienceContext } from 'context/workExperience'
+import { useEducationContext } from 'context/education'
 import UserInfoBar from 'components/UserInfoBar'
 import ReferencesSection from 'components/ReferencesSection'
 import SkillsSection from 'components/SkillsSection'
 import WorkExperienceSection from 'components/WorkExperienceSection'
 import EducationSection from 'components/EducationSection'
-import { fetchAndSet } from 'services/utils'
 
 export default function Home() {
-  const [user, setUser] = useState(null)
-  const [references, setReferences] = useState(null)
-  const [skills, setSkills] = useState(null)
-  const [workExperience, setWorkExperience] = useState(null)
-  const [education, setEducation] = useState(null)
-
-  useSWR('api/user', fetchAndSet(setUser, 'user'))
-  useSWR('api/references', fetchAndSet(setReferences, 'references'))
-  useSWR('api/skills', fetchAndSet(setSkills, 'skills'))
-  useSWR('api/work-experience', fetchAndSet(setWorkExperience, 'workExperience'))
-  useSWR('api/education', fetchAndSet(setEducation, 'education'))
+  const [user, setUser] = useUserContext()
+  const [references, setReferences] = useReferencesContext()
+  const [skills, setSkills] = useSkillsContext()
+  const [workExperience, setWorkExperience] = useWorkExperienceContext()
+  const [education, setEducation] = useEducationContext()
 
   return (
     <div className="mx-8 py-8 h-full md:h-[1485px] md:flex md:flex-col md:overflow-hidden">
