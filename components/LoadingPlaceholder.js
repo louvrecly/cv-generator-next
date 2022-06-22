b9dd33a79dfea642c52fa4f40b1f258f6b69ecce
Louvre Chow <louvrecly@gmail.com>
2022-06-22 16:49:41 -0400

    Style textarea

--- components/LoadingPlaceholder.js
+++ components/LoadingPlaceholder.js
@@ -33,7 +33,7 @@ export default function LoadingPlaceholder({ children, data, placeholder = 'Load
           : editing
             ? (
               <form className="py-1" onSubmit={handleSubmit}>
-                <textarea name="data" className="w-full text-xs" placeholder="Input JSON here..." rows="6" defaultValue={value} onChange={handleInput} />
+                <textarea name="data" className="px-3 py-2 w-full text-xs border border-neutral-300 rounded" placeholder="Input JSON here..." rows="6" defaultValue={value} onChange={handleInput} />
 
                 <div className="py-1 flex justify-center gap-x-3">
                   <button className="c-button c-button--green" type="submit">Submit</button>
