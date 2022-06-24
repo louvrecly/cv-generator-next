887690bc0fa95dfdcc80ac8bf186c8bb93858f4d
Louvre Chow <louvrecly@gmail.com>
2022-06-23 22:01:37 -0400

    Swap WorkExperienceSection with EducationSection on home page

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import { useUserContext } from 'context/user'
 import { useReferencesContext } from 'context/references'
 import { useSkillsContext } from 'context/skills'
-import { useWorkExperienceContext } from 'context/workExperience'
 import { useEducationContext } from 'context/education'
+import { useWorkExperienceContext } from 'context/workExperience'
 import UserInfoBar from 'components/UserInfoBar'
 import ReferencesSection from 'components/ReferencesSection'
 import SkillsSection from 'components/SkillsSection'
-import WorkExperienceSection from 'components/WorkExperienceSection'
 import EducationSection from 'components/EducationSection'
+import WorkExperienceSection from 'components/WorkExperienceSection'
 
 export default function Home() {
   const [user, setUser] = useUserContext()
   const [references, setReferences] = useReferencesContext()
   const [skills, setSkills] = useSkillsContext()
-  const [workExperience, setWorkExperience] = useWorkExperienceContext()
   const [education, setEducation] = useEducationContext()
+  const [workExperience, setWorkExperience] = useWorkExperienceContext()
 
   return (
     <div className="mx-8 py-8 h-full md:h-[1485px] md:flex md:flex-col md:overflow-hidden">
@@ -25,9 +25,9 @@ export default function Home() {
 
         <SkillsSection skills={skills} setSkills={setSkills} />
 
-        <WorkExperienceSection workExperience={workExperience} setWorkExperience={setWorkExperience} />
-
         <EducationSection education={education} setEducation={setEducation} />
+
+        <WorkExperienceSection workExperience={workExperience} setWorkExperience={setWorkExperience} />
       </div>
     </div>
   )
