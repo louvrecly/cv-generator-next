02033710f70ac6be9cdec9b8ca59ddcd53a90e21
Louvre Chow <louvrecly@gmail.com>
2022-06-30 22:37:37 -0400

    Replace all other contexts with DataContext on home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import { useUserContext } from 'context/user'
-import { useReferencesContext } from 'context/references'
-import { useSkillsContext } from 'context/skills'
-import { useEducationContext } from 'context/education'
-import { useWorkExperienceContext } from 'context/workExperience'
-import { useProjectsContext } from 'context/projects'
+import { useContext, useEffect } from 'react'
+import DataContext from 'context/data'
 import UserInfoBar from 'components/UserInfoBar'
 import ReferencesSection from 'components/ReferencesSection'
 import SkillsSection from 'components/SkillsSection'
@@ -12,12 +8,30 @@ import WorkExperienceSection from 'components/WorkExperienceSection'
 import ProjectsSection from 'components/ProjectsSection'
 
 export default function Home() {
-  const [user, setUser] = useUserContext()
-  const [references, setReferences] = useReferencesContext()
-  const [skills, setSkills] = useSkillsContext()
-  const [education, setEducation] = useEducationContext()
-  const [workExperience, setWorkExperience] = useWorkExperienceContext()
-  const [projects, setProjects] = useProjectsContext()
+  const { state, setters } = useContext(DataContext)
+  const { user, references, skills, education, workExperience, projects } = state
+  const { setUser, setReferences, setSkills, setEducation, setWorkExperience, setProjects } = setters
+
+  useEffect(() => {
+    const payloads = [
+      { endpoint: 'user', field: 'user', setter: setUser },
+      { endpoint: 'references', field: 'references', setter: setReferences },
+      { endpoint: 'skills', field: 'skills', setter: setSkills },
+      { endpoint: 'education', field: 'education', setter: setEducation },
+      { endpoint: 'work-experience', field: 'workExperience', setter: setWorkExperience },
+      { endpoint: 'projects', field: 'projects', setter: setProjects }
+    ]
+
+    Promise.all(
+      payloads.map(
+        payload =>
+          fetch(`api/${payload.endpoint}`)
+            .then(res => res.json())
+            .then(data => data[payload.field] && payload.setter(data[payload.field]))
+            .catch(error => `Error - ${error.message}`)
+      )
+    )
+  }, [])
 
   return (
     <div className="mx-8 py-8 h-full md:h-[1485px] md:flex md:flex-col md:overflow-hidden">
