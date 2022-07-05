7093e428950c27be147fedddb138021583e5f2b2
Louvre Chow <louvrecly@gmail.com>
2022-07-05 19:56:43 -0400

    Create ActivitiesSection and import ActivitiesTable to it

---
+++ components/ActivitiesSection/index.js
@@ -0,0 +1,14 @@
+import Section from 'components/Section'
+import EditableBlock from 'components/EditableBlock'
+import ActivitiesTable from './ActivitiesTable'
+import { filterByShow } from 'services/utils'
+
+export default function ActivitiesSection({ activities, setActivities }) {
+  return (
+    <Section title="Activities">
+      <EditableBlock data={activities} setData={setActivities} placeholder="Loading activities data...">
+        <ActivitiesTable data={filterByShow(activities)} />
+      </EditableBlock>
+    </Section>
+  )
+}
