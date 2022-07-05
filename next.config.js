dee702522d7279641ecddbf4a1ceaa0a0cc77b5c
Louvre Chow <louvrecly@gmail.com>
2022-07-04 21:45:55 -0400

    Add domains in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'upload.wikimedia.org',
+      'openseauserdata.com',
+      'images.vexels.com',
+      'media-exp1.licdn.com',
+      'pigeoncity.com.hk'
+    ]
+  }
 }
 
 module.exports = nextConfig
