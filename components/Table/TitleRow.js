cd9d9a9644143843a2609a8e05869eef9a4f5c69
Louvre Chow <louvrecly@gmail.com>
2022-07-05 17:27:31 -0400

    Create TitleRow component

---
+++ components/Table/TitleRow.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function TitleRow({ title, logo = null }) {
+  return (
+    <div className="flex items-center gap-2">
+      {logo && <Image src={logo} alt={title} width={15} height={15} />}
+
+      <h3 className="font-bold text-sm">{title}</h3>
+    </div>
+  )
+}
