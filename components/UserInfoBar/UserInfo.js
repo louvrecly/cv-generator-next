5887bde4a6a7f4e33ccd4b80d313de2aa8042c54
Louvre Chow <louvrecly@gmail.com>
2023-01-15 15:54:11 -0500

    Add icon field to contacts field in user.json and render FontAwesomeIcon in the UserInfo if contact.icon is truthy

--- components/UserInfoBar/UserInfo.js
+++ components/UserInfoBar/UserInfo.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function UserInfo({ data }) {
   return (
     <div className="w-full flex flex-wrap justify-stretch items-center gap-x-3">
@@ -6,8 +8,10 @@ export default function UserInfo({ data }) {
       {data.contacts && (
         <ul>
           {data.contacts.map(contact => (
-            <li key={contact.key} className="flex justify-between text-xs">
-              <span className="mr-1 font-bold">{contact.label}:</span>
+            <li key={contact.key} className="flex justify-between items-center gap-2 text-xs">
+              {contact.icon
+                ? <FontAwesomeIcon icon={`fa-solid fa-${contact.icon}`} />
+                : <span className="mr-1 font-bold">{contact.label}:</span>}
               <span>{contact.value}</span>
             </li>
           ))}
