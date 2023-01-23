a947311ee4365325e9c83d14fd36d39486da0eaa
Louvre Chow <louvrecly@gmail.com>
2023-01-22 19:48:48 -0500

    Refactor items from strings to objects with show property in activities, education, portfolio and work-experience

--- components/Table/ItemsList.js
+++ components/Table/ItemsList.js
@@ -1,7 +1,11 @@
 export default function ItemsList({ items = [] }) {
   return (
     <ul className="list-disc list-inside text-xs">
-      {items.map((item, idx) => <li key={idx} className="py-px text-slate-500">{item}</li>)}
+      {items
+        .filter(item => item.show)
+        .map((item, idx) => (
+          <li key={idx} className="py-px text-slate-500">{item.content}</li>
+        ))}
     </ul>
   )
 }
