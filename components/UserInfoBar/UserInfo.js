bef40dfcd555a9997015751e3169d8f0d0e507fa
Louvre Chow <louvrecly@gmail.com>
2023-06-11 22:24:14 -0400

    Add icon.style in the UserInfo

--- components/UserInfoBar/UserInfo.js
+++ components/UserInfoBar/UserInfo.js
@@ -1,19 +1,22 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Linkable from 'components/Table/Linkable'
 
 export default function UserInfo({ data }) {
   return (
     <div className="w-full flex flex-wrap justify-stretch items-center gap-x-3">
-      <h1 className="flex-1 text-xl uppercase whitespace-nowrap">{data.name}</h1>
+      <h1 className="flex-1 text-2xl whitespace-nowrap">{data.name}</h1>
 
       {data.contacts && (
         <ul>
           {data.contacts.map(contact => (
-            <li key={contact.key} className="flex justify-between items-center gap-2 text-xs">
-              {contact.icon
-                ? <FontAwesomeIcon icon={`fa-solid fa-${contact.icon}`} />
-                : <span className="mr-1 font-bold">{contact.label}:</span>}
-              <span>{contact.value}</span>
-            </li>
+            <Linkable key={contact.key} link={contact.link}>
+              <li className={`flex ${contact.icon ? 'flex-row-reverse' : 'justify-between'} items-center gap-2 text-xs`}>
+                {contact.icon
+                  ? <FontAwesomeIcon icon={`fa-${contact.icon.style} fa-${contact.icon.name}`} />
+                  : <span className="mr-1 font-bold">{contact.label}:</span>}
+                  <span>{contact.value}</span>
+              </li>
+            </Linkable>
           ))}
         </ul>
       )}
